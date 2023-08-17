26261cf3d2b6fee69c958a5b97655b0a498e8e6a
RobertAzatovichSitdikov <kotiknumbertwo@gmail.com>
2023-08-17 15:45:03 +0300

    test

--- test.java
+++ test.java
@@ -1,5 +1,5 @@
 public class test {
     public static void main(String[] args) {
-        System.out.println("Hello world! Good bye!");
+        System.out.println("Hello world! Good bye!test");
     }
 }GIT
