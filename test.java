8535011053d2dd140bd268ad54632fdc6dbe0268
RobertAzatovichSitdikov <kotiknumbertwo@gmail.com>
2023-08-17 14:27:37 +0300

    add good bye to file1

--- test.java
+++ test.java
@@ -1,5 +1,5 @@
 public class test {
     public static void main(String[] args) {
-        System.out.println("Hello world!Niga Негр");
+        System.out.println("Hello world! Good bye!");
     }
-}
+}GIT
